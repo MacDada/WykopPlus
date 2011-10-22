0f382f989c4c654d62c86df2cd430ede98d61e97
Dawid Nowak (MacDada) <contact@dnowak.pl>
2011-10-22 14:25:35 +0200

    dzialajaca beta skryptu

---
+++ wykop_plus.user.js
@@ -0,0 +1,135 @@
+// ==UserScript==
+// @name           Wykop Plus
+// @namespace      http://userscripts.org/users/364624
+// @include        http://www.wykop.pl/
+// @include        http://www.wykop.pl/strona/*
+// @include        http://www.wykop.pl/wykopalisko/*
+// ==/UserScript==
+
+
+/**
+ * Hack: 
+ * - loading jQuery from Window on FF,
+ * - on Chrome jQuery is already loaded by TamperMonkey.
+ */
+if ('undefined' == typeof $) {
+	$ = unsafeWindow.$;
+}
+
+	
+$(function() {
+
+	var isNightThemeOn = ("rgb(28, 28, 28)" == $("body").css("background-color"));
+	var hide_article_buttons = null;
+
+
+	/**
+	 * Check if an element exists in given array
+	 * using a comparer function.
+	 *
+	 * comparer : function(currentElement)
+	 */
+	Array.prototype.inArray = function(comparer) { 
+	    for (var i=0; i < this.length; i++) { 
+	        if (comparer(this[i])) {
+	        	return true;
+	        }
+	    }
+	    return false; 
+	}; 
+	
+
+	/**
+	 * Adds an element to the array, 
+	 * if it does not already exist,
+	 * using a comparer function.
+	 */
+	Array.prototype.pushIfNotExists = function(element, comparer) { 
+	    if (!this.inArray(comparer)) {
+	        this.push(element);
+	    }
+	};
+	
+
+	/**
+	 * Pobiera z pamięci przeglądarki linki do artykułów,
+	 * które mają nie być wyświetlane.
+	 */
+	function getHiddenArticlesUrls() {
+		try {
+			var urls = JSON.parse(localStorage.getItem("wp_hidden_articles"));
+			if (!(('object' == typeof(urls)) && (urls instanceof Array))) {
+				urls = [];
+			}
+		} catch(e) {
+			var urls = [];
+		}
+		return urls;	
+	}
+
+
+	/**
+	 * Zapisuje w lokalnej pamięci przeglądarki URL do artykułu,
+	 * który nie ma być wyświetlany.
+	 */
+	function addHiddenArticleUrl(url) {
+		urls = getHiddenArticlesUrls();
+		
+		urls.pushIfNotExists(url, function(e) {
+			return (url == e);
+		});
+		
+		localStorage.setItem("wp_hidden_articles", JSON.stringify(urls));
+	};
+	
+	
+	/**
+	 * Ukrywam linki z listy na starcie.
+	 */
+	$("article.entry .content header h2 a.link").each(function() {
+		o = $(this);
+		var urls = getHiddenArticlesUrls();
+		
+		if (urls.inArray(function(e) {
+			return (o.attr("href") == e)
+		})) {
+			o.parents("article.entry").hide().addClass("wp_hidden_article");
+		}
+	});
+	
+	
+	/**
+	 * Wyświetlam lub ukrywam linki w zależności od checkboxa.
+	 */
+	$('<label for="wp_hidden_articles" style="float:right;font-size:10px;margin-right:10px;">pokaż ukryte</label><input type="checkbox" name="wp_show_hidden_articles" style="float:right;margin: 3px 3px 0px 0px;" />')
+	.insertAfter(".filters .slidemenu.categories")
+	.change(function() {
+		if ("checked" == $(this).attr("checked")) {
+			$("article.entry.wp_hidden_article").fadeIn();
+		} else {
+			$("article.entry.wp_hidden_article").fadeOut();	
+		}
+	});
+	
+	
+	/**
+	 * Ukrywam link po wskazaniu przez użytkownika.
+	 */
+	hide_article_buttons = $('<a href="#ukryjArtykuł" style="margin-left: 10px; font-size: 10px; color: ' + ((isNightThemeOn) ? 'grey' : 'darkBlue') + ';">nie pokazuj więcej</a>')
+	.insertAfter("article.entry .content header h2 a.link")
+	.click(function(e) {
+		e.preventDefault();
+		addHiddenArticleUrl($(this).prev("a.link").attr("href"));
+		$(this).parents("article.entry").fadeOut().addClass("wp_hidden_article");
+	});
+	
+	
+	/**
+	 * Przycisk ukrywania wszystkich artykułów na stronie.
+	 */
+	$('<a href="#" style="float: right; margin-right: 10px; font-size: 9px;">ukryj wszystkie</a>').insertAfter("input[name='wp_show_hidden_articles']").click(function(e) {
+		e.preventDefault();
+		hide_article_buttons.trigger("click");
+	});
+	
+});
